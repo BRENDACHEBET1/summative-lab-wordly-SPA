7ac278c995c96045d12168b4f1c4fe1964e5b5d2
BRENDACHEBET1 <chebetb388@gmail.com>
2026-04-11 01:31:35 +0300

    Initial Commit

---
+++ script.js
@@ -0,0 +1,179 @@
+// Getting elements
+const form = document.getElementById("form");
+const input = document.getElementById("text-input");
+
+const image = document.getElementById("initial-image");
+const loading = document.getElementById("loading-state");
+const resultBox = document.getElementById("result-state");
+const errorBox = document.getElementById("error-state");
+
+const wordInput = document.getElementById("word");
+const pronunciation = document.getElementById("pronunciation");
+const definitionOutput = document.getElementById("definitions");
+const audioOutput = document.getElementById("audio");
+const errorMessage = document.getElementById("error-message");
+const speakerBtn = document.getElementById("speakerBtn");
+const saveBtn = document.getElementById("saveBtn");
+const savedWordsList = document.getElementById("saved-words");
+
+
+
+// Function to control what is visble
+// takes state as aparameter and switches bewteen states
+function setState(state) {
+  image.classList.add("hidden");
+  loading.classList.add("hidden");
+  resultBox.classList.add("hidden");
+  errorBox.classList.add("hidden");
+
+  if (state === "idle") image.classList.remove("hidden");
+  if (state === "loading") loading.classList.remove("hidden");
+  if (state === "result") resultBox.classList.remove("hidden");
+  if (state === "error") errorBox.classList.remove("hidden");
+}
+
+// Validate input:Numeric and special charactrs
+function isValidWord(word) {
+  return /^[a-zA-Z']+$/.test(word);
+}
+
+// ASync function to fetch word
+
+async function fetchWord(word) {
+  setState("loading");
+
+  try {
+   if (!isValidWord(word)) {
+      throw new Error("invalid"); //caught in catch block to show invalid word
+    }
+
+    // getting word
+    const res = await fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${word}`
+      
+    );
+
+    if (!res.ok) throw new Error("notfound"); //catch block to display invalid
+
+    const data = await res.json(); //parsing data
+    // check if data exists
+    if (!data || data.length === 0) {
+  throw new Error("notfound");
+  }
+    const entry = data[0];
+
+    // WORD
+    wordInput.textContent = entry.word || word;
+
+    // PRONUNCIATION
+    let phonetic = "";
+
+    if (entry.phonetic) {
+      phonetic = entry.phonetic;
+    } else if (entry.phonetics && entry.phonetics.length > 0) {
+      for (let i = 0; i < entry.phonetics.length; i++) {
+        if (entry.phonetics[i].text) {
+          phonetic = entry.phonetics[i].text;
+          break;
+        }
+      }
+    }
+
+    if (phonetic) {
+      pronunciation.textContent = "/" + phonetic + "/";
+    } else {
+      pronunciation.textContent = "";
+    }
+
+    // AUDIO 
+    let audio = "";
+
+    if (entry.phonetics && entry.phonetics.length > 0) {
+      for (let i = 0; i < entry.phonetics.length; i++) {
+        if (entry.phonetics[i].audio && entry.phonetics[i].audio !== "") {
+          audio = entry.phonetics[i].audio;
+          break;
+        }
+      }
+    }
+
+    audioOutput.src = audio;
+
+    // DEFINITIONS 
+    const meanings = entry.meanings || [];
+    const defs = meanings[0]?.definitions || [];
+
+    definitionOutput.innerHTML = defs
+      .slice(0, 2)
+      .map(d => "<li>" + d.definition + "</li>")
+      .join("");
+
+    setState("result");
+
+    // clear input field after results are displayed
+input.value = "";
+
+  } catch (err) {
+    if (err.message === "invalid") {
+      errorMessage.textContent = "Invalid word. Please enter letters only.";
+    } else if (err.message === "notfound") {
+      errorMessage.textContent = "Word not found.";
+    } else {
+      errorMessage.textContent = "Something went wrong. Try again";
+    }
+    setState("error");
+  }
+}
+
+// Prevent page from reloading
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const word = input.value.trim();
+  if (!word) return;
+
+  fetchWord(word);
+});
+
+
+// connecting audio to speaker
+speakerBtn.addEventListener("click", () => {
+  if (audioOutput.src) {
+    audioOutput.play();
+  }
+});
+
+
+// Save words
+let savedWords = JSON.parse(localStorage.getItem("words")) || [];
+
+function renderSavedWords() {
+  savedWordsList.innerHTML = savedWords
+    .map(word => `<li>${word}</li>`)
+    .join("");
+}
+// adding event listener to save button and limiting displayed words to three
+saveBtn.addEventListener("click", () => {
+  const currentWord = wordInput.textContent;
+
+  if (!currentWord) return;
+
+  if (savedWords.includes(currentWord)) return;
+
+  if (savedWords.length >= 3) {
+    savedWords.shift(); // remove oldest
+  }
+
+  savedWords.push(currentWord);
+  localStorage.setItem("words", JSON.stringify(savedWords)); //local storage, stringfy converts aray to strings
+  renderSavedWords(); //reloads the saved words
+});
+
+// functionality to click on saved words and show definition
+function renderSavedWords() {
+  savedWordsList.innerHTML = savedWords
+    .map(word => `<li onclick="fetchWord('${word}')">${word}</li>`)
+    .join("");
+}
+
+
+// setState("idle");
